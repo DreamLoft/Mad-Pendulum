85b2219f18dd8a62e120840b78db695c93de94c1
Rakesh Sharma <sharma26.riti@gmail.com>
2017-08-11 12:23:50 +0530

    Update welcome.js

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,4 +1,4 @@
-var url= "http://ec2-35-154-49-51.ap-south-1.compute.amazonaws.com/api/data";
+var url= "http://ec2-13-126-51-238.ap-south-1.compute.amazonaws.com/api/data";
 dataRequest(url);
 function dataRequest(url){
   d3.json(url, function (err, data) {
