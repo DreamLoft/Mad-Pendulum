6ad0572ff211634c252c4ef25cc841ae5cf4e311
Rakesh Sharma <sharma26.riti@gmail.com>
2017-09-09 13:20:49 +0530

    Update scheduler.rb

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,9 +2,9 @@ require 'rufus-scheduler'
 
 
 s = Rufus::Scheduler.singleton
-# s.every '24h' , :first_at => "#{Date.today} 23:05:00 +0530" do
- # @admins= User.select{|u| u.isadmin== true}
-  # @admins.each do |admin|
-   # UserMailer.report_email(admin).deliver_now
- # end
-# end
+ s.every '24h' , :first_at => "#{Date.today} 23:05:00 +0530" do
+  @admins= User.select{|u| u.isadmin== true}
+   @admins.each do |admin|
+    UserMailer.report_email(admin).deliver_now
+  end
+ end
