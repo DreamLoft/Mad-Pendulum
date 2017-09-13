16ec2353e66697152c15d39b8427643e9e517ce8
DreamLoft <31210381+DreamLoft@users.noreply.github.com>
2017-09-13 12:22:57 +0530

    Reporting Email will no include disabled users

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def report_email(admin)
    @admin = admin
-   @users= User.select{|u| u.Sbu == @admin.Sbu && u.onLeave != true }
+   @users= User.select{|u| u.Sbu == @admin.Sbu && u.onLeave != true && u.approved != false }
    @timesheets= Timesheet.select{|t| t.Tdate== Date.today}
    @leftuser= @users.reject{|u| @timesheets.map{|t| t.user_id }.include? u.id }
    mail(to: @admin.email, subject: 'Timesheet Report')
