00e049b0238a3fd79765e3a53c42d50e0a5029ed
shhhRakesh <sharma26.riti@gmail.com>
2018-01-03 19:11:32 +0530

    Data Load Reduced

--- app/controllers/api/timesheets_controller.rb
+++ app/controllers/api/timesheets_controller.rb
@@ -3,7 +3,17 @@ class Api::TimesheetsController < Api::ApplicationController
   #before_action :authenticate_user!, only: [:index, :show, :new, :edit, :create, :update , :destroy]
   #skip_before_action :verify_authenticity_token ,only: [:index, :show, :new, :edit, :create, :update , :destroy]
 def index
-  @timesheets= Timesheet.all
+#  @timesheets= Timesheet.all
+  #  @timesheets= []
+    if (current_user.is_project_manager)
+      pros= Project.where("project_manager = ? OR project_lead= ? ", current_user.id , current_user.id).pluck(:id)
+      @timesheets= Timesheet.where(project_id: pros)
+    elsif (current_user.is_project_lead)
+      pros= Project.where("project_manager = ? OR project_lead= ? ", current_user.id , current_user.id).pluck(:id)
+      @timesheets= Timesheet.where(project_id: pros)
+    else
+      @timesheets= Timesheet.where('user_id' => current_user.id)
+    end
     render json: @timesheets
 end
 
@@ -28,7 +38,7 @@ def create
   rescue => e
     render json: "An Error Occured Please Try Again Later".to_json, status: :conflict
 end
-     
+
 def update
     @timesheet = Timesheet.find(params[:id])
     if @timesheet.update(timesheet_params)
